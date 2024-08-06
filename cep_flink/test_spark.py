d00ef81205e6dea11dbd9b4944c10abeb9d23813
김병민 <akfkdhwk@gmail.com>
2024-08-06 12:57:03 +0900

    디렉토리 삭제

--- cep_flink/test_spark.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-# Spark 세션 생성
-spark = SparkSession.builder \
-    .appName("TestSparkSession") \
-    .master("local") \
-    .getOrCreate()
-
-print("Spark session created successfully.")
